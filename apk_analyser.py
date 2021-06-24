c3119ece0a7b24628decced0c0f82bba1f88bc4e
Robert-Gabriel Chiper <rxc691@cs.bham.ac.uk>
2021-06-24 15:36:09 +0300

    Added functionality to work with local file without downloading instead of only package_names.json with download

--- apk_analyser.py
+++ apk_analyser.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import sys
 from glob import glob
 
 from cp55.apk_handler import ApkHandler
@@ -30,58 +31,87 @@ def download_apk(package_name):
     os.rename(downloaded_apk_file, apk_file)
 
 
+def process_apk(apk_path, input_package, inspection_filter, db):
+    apk_handler = ApkHandler(apk_path)
+
+    try:
+        apk_handler.decode_apk()
+
+        component_inspector = ComponentInspector(apk_handler, inspection_filter)
+        background_results, analysis_status = component_inspector.inspect_background_components()
+
+        app_id = db.insert_app(input_package, analysis_status)
+        db.insert_components(app_id, background_results)
+
+        if analysis_status == "full":
+            os.remove(apk_path)
+            print("Finished analyzing app " + input_package + " with status " + analysis_status +
+                  ". Summary: " + str(len(background_results)) + " component(s).")
+
+        elif analysis_status == "background":
+            sql_results = component_inspector.inspect_providers_for_sql_injection()
+
+            db.insert_sql_checks(app_id, sql_results)
+            db.update_app_analysis_status(app_id, "full")
+
+            print("Finished analyzing app " + input_package + " with status " + analysis_status +
+                  ". Summary: " + str(len(background_results)) + " component(s). Also analyzed " +
+                  str(len(sql_results)) + " methods for sql vulnerabilities.")
+
+        apk_handler.cleanup()
+
+    except Exception:
+        apk_handler.cleanup()
+        db.insert_app(input_package, "failed")
+        print("Failed to inspect app " + input_package + " or to store the results in the database.")
+        raise Exception
+
+
 def main():
     db = DatabaseInterface()
 
-    package_list_file = open("package_names.json", "r")
-    packages = json.load(package_list_file)
-
     inspection_filter_file = open("filter.json", "r")
-    inspection_filer = json.load(inspection_filter_file)
+    inspection_filter = json.load(inspection_filter_file)
 
-    for input_package in packages:
-        try:
-            download_apk(input_package)
-        except Exception:
-            db.insert_app(input_package, "download_failed")
-            print("Failed to download app " + input_package + ".")
-            continue
+    if len(sys.argv) < 2:
+        # Using the package_names.json file
+        print("Working with the package_names.json file.")
 
-        apk_path = output_directory + input_package + apk_file_extension
-        apk_handler = ApkHandler(apk_path)
+        package_list_file = open("package_names.json", "r")
+        packages = json.load(package_list_file)
 
-        try:
-            apk_handler.decode_apk()
+        for input_package in packages:
+            try:
+                download_apk(input_package)
+            except Exception:
+                db.insert_app(input_package, "download_failed")
+                print("Failed to download app " + input_package + ".")
+                continue
 
-            component_inspector = ComponentInspector(apk_handler, inspection_filer)
-            background_results, analysis_status = component_inspector.inspect_background_components()
+            apk_path = output_directory + input_package + apk_file_extension
 
-            app_id = db.insert_app(input_package, analysis_status)
-            db.insert_components(app_id, background_results)
-
-            if analysis_status == "full":
+            try:
+                process_apk(apk_path, input_package, inspection_filter, db)
+            except Exception:
                 os.remove(apk_path)
-                print("Finished analyzing app " + input_package + " with status " + analysis_status +
-                      ". Summary: " + str(len(background_results)) + " component(s).")
+                continue
+    else:
+        # Using a local apk file
+        print("Working with a local apk.")
 
-            elif analysis_status == "background":
-                sql_results = component_inspector.inspect_providers_for_sql_injection()
+        apk_path = sys.argv[1]
 
-                db.insert_sql_checks(app_id, sql_results)
-                db.update_app_analysis_status(app_id, "full")
+        if not os.path.exists(apk_path):
+            print("The argument provided is not a valid file path.")
+            exit(0)
 
-                print("Finished analyzing app " + input_package + " with status " + analysis_status +
-                      ". Summary: " + str(len(background_results)) + " component(s). Also analyzed " +
-                      str(len(sql_results)) + " methods for sql vulnerabilities.")
+        if not apk_path.endswith(".apk"):
+            print("The argument provided does not have the '.apk' file extension.")
+            exit(0)
 
-            apk_handler.cleanup()
+        input_package = apk_path.split("/")[-1][:-4]
 
-        except Exception:
-            apk_handler.cleanup()
-            os.remove(apk_path)
-            db.insert_app(input_package, "failed")
-            print("Failed to inspect app " + input_package + " or to store the results in the database.")
-            continue
+        process_apk(apk_path, input_package, inspection_filter, db)
 
 
 if __name__ == "__main__":
